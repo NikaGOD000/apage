b387a074f6ea7f6997b33fd6314d3112752b54e9
NikaGOD000 <166658690+NikaGOD000@users.noreply.github.com>
2025-07-11 04:02:46 +0700

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className }) {
-  return <div className={`bg-white bg-opacity-10 rounded-xl ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children, className }) {
-  return <div className={`p-4 ${className}`}>{children}</div>;
-}
