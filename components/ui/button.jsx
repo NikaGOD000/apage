3a543ad7971417b53fbb6b283e8446f361391b9b
NikaGOD000 <166658690+NikaGOD000@users.noreply.github.com>
2025-07-11 04:14:41 +0700

    Delete components/ui directory

--- components/ui/button.jsx
+++
@@ -1,8 +0,0 @@
-export function Button({ children, onClick, variant }) {
-  const style = variant === "ghost" ? "bg-transparent hover:bg-white hover:bg-opacity-20" : "bg-white";
-  return (
-    <button onClick={onClick} className={`text-white p-2 rounded-full ${style}`}>
-      {children}
-    </button>
-  );
-}
